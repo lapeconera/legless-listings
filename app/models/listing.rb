0b5fd81084e58962225aacbd6f49b57c62a958b3
Dodam <lapeconera@gmail.com>
2019-04-26 10:41:29 +1000

    added in stripe gem and modified show viw with deposit button

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,6 @@ class Listing < ApplicationRecord
   enum sex: { female: 0, male: 1 }
   validates :title, :breed_id, :sex, :price, :deposit, :date_of_birth, presence: true
   has_one_attached :picture
-
+  belongs_to :user
 
 end
